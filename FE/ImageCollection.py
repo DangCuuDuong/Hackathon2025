ae9c681d2739921f64bf00d853de06f979b2935c
DangCuuDuong <duongdangnt@gmail.com>
2025-04-05 21:15:19 +0700

    update FE

---
+++ FE/ImageCollection.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import os
+import pandas as pd
+
+
+
+def ImageCollection():
+    st.subheader("Upload Image to Identify Ingredients")
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    return f"{uploaded_file.name}"  # Tên file được upload
